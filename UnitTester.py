b6d797c592311665dd76abad1bf391ecce36b19f
michaelelcock <michaelelcock@hotmail.com>
2016-09-28 16:29:21 +1300

    Bad Smell 3, Lazy Class tests
    Wrote tests for final bad smell, lazy class

--- UnitTester.py
+++ UnitTester.py
@@ -57,10 +57,23 @@ class UnitTesting(unittest.TestCase):
         actual = self.my_scraper.get_nat_dex()
         self.assertListEqual(expected, actual)
     """
-    def test_4scrape_without_comma_remover(self):
+    def test_3scrape_without_comma_remover(self):
         print("test3")
         expected = [['001', 'Bulbasaur', 'Grass', 'Poison',
                      'http://pokemondb.net/pokedex/Bulbasaur', 'Seed Pokemon',
+                     '0.71m', '6.9 kg', '001 (Red,Blue,Yellow,FireRed,'
+                     'LeafGreen)226 (Gold,Silver,Crystal)231'
+                     ' (HeartGold,SoulSilver)080 (X,Y)']]
+        self.my_scraper.set_generation(0)
+        self.my_scraper.web_scraper()
+        actual = self.my_scraper.get_nat_dex()
+        self.assertListEqual(expected, actual)
+
+    def test_4scraper_subclass(self):
+        print("test3")
+        expected = [['001', 'Bulbasaur', 'Grass', 'Poison',
+                     'http://pokemondb.net/pokedex/Bulbasaur',
+                     'Seed Pokemon',
                      '0.71m', '6.9 kg', '001 (Red,Blue,Yellow,FireRed,'
                      'LeafGreen)226 (Gold,Silver,Crystal)231'
                      ' (HeartGold,SoulSilver)080 (X,Y)']]
